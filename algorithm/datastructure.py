15838e3c0c6543476173d2be7cc5b5f92ce1977e
Yajing Tang <yajingt@andrew.cmu.edu>
2013-11-02 15:22:53 -0400

    Added workflow of our algorithm and framework of data structure

---
+++ algorithm/datastructure.py
@@ -0,0 +1,12 @@
+# preprocessed data
+class PreData:
+	self.id; #int represent the abstract
+	self.disease;#string
+	self.sentence; #string
+	self.graph; #dictionary
+# graph
+class DiseaseGraph:
+	self.disease;
+	self.graph;
+
+	
